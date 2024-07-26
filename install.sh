e8599c35a5ac6de6928b192cb3a7422076e29a22
Jakob Fiegerl <jakob.fiegerl@gmail.com>
2024-07-26 21:00:06 +0200

    work in progress

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 go build -o build/stacks main.go
-path=$(pwd)
-echo "alias stacks='$path/build/stacks'" >> ~/.zshrc
-echo "Stacks has been added to your aliases. Restart your terminal and type 'stacks' to run the program."
+src=$(pwd)
+echo "export PATH=\"$src/build:\$PATH\"" >> ~/.zshrc
+echo "Stacks has been installed succesfully. Restart your terminal and type 'stacks' to run the program."
