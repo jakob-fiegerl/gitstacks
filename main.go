dbe9bd75c3e0e797c2f20ebe28dec2ac09402406
Jakob Fiegerl <jakob.fiegerl@gmail.com>
2024-07-26 20:40:08 +0200

    work in progress

--- main.go
+++ main.go
@@ -154,6 +154,7 @@ func commands(app *cli.App) {
 				} else {
 					baseCommand += " && git stash pop"
 				}
+				fmt.Println(baseCommand)
 				fmt.Println(internal.ExecuteCommand(baseCommand))
 				return nil
 			},
